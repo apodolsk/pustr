7a2a56302941cbaba9db949494e4623dd1a1aa7e
Alex V. Podolsky <apodolsk@andrew.cmu.edu>
2015-10-30 17:41:53 -0400

    Added standalone log flag.

--- atomics.h
+++ atomics.h
@@ -100,3 +100,7 @@ bool _upd2_won(dptr n, volatile dptr *p, dptr *old);
 
 #define pudef (howok, "%", *a == WON ? "WON" : *a == OK ? "OK" : "NOT")
 #include <pudef.h>
+
+#ifndef LOG_ATOMICS
+#define LOG_ATOMICS 0
+#endif 
