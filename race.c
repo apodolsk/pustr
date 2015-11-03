1bd0a8eb17a26874c85e758d2f7db82d0bcf8b41
Alex V. Podolsky <apodolsk@andrew.cmu.edu>
2015-11-03 12:57:32 -0500

    Fixed: yield case in race had been disabled by T->racing check. Duh.

--- race.c
+++ race.c
@@ -7,10 +7,13 @@ extern err kyield(tid t);
 void race(cnt maxns, uint pcnt, uint idmod){
     if(!pcnt)
         return;
+    bool r = T->racing;
     T->racing = true;
-    if(!T->racing && interrupts_enabled() && randpcnt(pcnt >> 1)){
-        (kyield)((dptr) -1);
-    }else if(!mod_pow2(PUN(uptr, get_dbg_id()), idmod) && randpcnt(pcnt))
-        nanosleep(&(struct timespec){.tv_nsec = rand() % maxns}, NULL);
+    if(!r && randpcnt(pcnt))
+        kyield((dptr) -1);
+    /* if(!T->racing && interrupts_enabled() && randpcnt(pcnt >> 1)){ */
+    /*     (kyield)((dptr) -1); */
+    /* }else if(!mod_pow2(PUN(uptr, get_dbg_id()), idmod) && randpcnt(pcnt)) */
+    /*     nanosleep(&(struct timespec){.tv_nsec = rand() % maxns}, NULL); */
     T->racing = false;
 }
