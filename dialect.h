a3849863a1b76fa7239a0b436250c460176f1cc8
Alex V. Podolsky <apodolsk@andrew.cmu.edu>
2016-01-11 14:29:38 -0500

    attrib visible->visible_fun

--- dialect.h
+++ dialect.h
@@ -15,7 +15,7 @@
 #define checked __attribute__((warn_unused_result))
 #define dbg __attribute__((unused))
 #define transparent __attribute__((transparent_union))
-#define visible __attribute__((visibility("protected")))
+#define visible_fun __attribute__((visibility("protected")))
 
 #include <whtypes.h>
 #include <limits.h>
